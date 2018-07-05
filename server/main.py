05392d0969d35568e73cfac501a2d1d0b514ea50
Stefan Kröner <sk7@users.noreply.github.com>
2018-07-05 16:32:29 +0200

    etc time as tracking id

--- server/main.py
+++ server/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, Response, request, redirect
 
 app = Flask(__name__)
@@ -7,7 +9,7 @@ def hello_world():
     to = request.args.get('to')
     redirect_to = redirect(to, code=302)
     response = app.make_response(redirect_to)
-    response.set_cookie('tracking_id', value='314159265359')
+    response.set_cookie('tracking_id', value=datetime.utcnow())
 
     return response
 
@@ -17,7 +19,7 @@ def hello_world():
 def test():
     cookie = request.cookies.get("tracking_id")
 
-    text = "tracking_id=%s" % cookie
+    text = "tracking_id=%s now=%s" % (cookie, datetime.utcnow())
     response = Response(response=text)
 
     return response
